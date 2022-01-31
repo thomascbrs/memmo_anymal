5439880958e36afe723e8b3900851d4ee5cf0220
thomasCbrs <thomas.corberes@gmail.com>
2022-01-31 19:31:08 +0000

    Add config file for SurfacePlanner

--- walkgen/__init__.py
+++ walkgen/__init__.py
@@ -1,5 +1,4 @@
 from .SurfacePlanner import SurfacePlanner
-from .SurfacePlannerURDF import SurfacePlannerURDF
 from .FootStepPlanner import FootStepPlanner
 from .GaitManager import GaitManager
 from .FootStepTrajectory import FootStepTrajectory
