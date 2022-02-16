cdb9ecd10e5cbc59a031d45039fb500835b832de
thomasCbrs <thomas.corberes@gmail.com>
2022-02-16 15:12:52 +0000

    Create Conversion class for ROS messages

---
+++ walkgen/WalkgenRosMessageConversion.py
@@ -0,0 +1,152 @@
+import rospy
+from footstep_msgs.msg import SetSurfaces, FootSurfaces, ConvexSurface, GaitStatusNewPhase
+from std_msgs.msg import Float64MultiArray, MultiArrayDimension
+from walkgen.tools.geometry_utils import Surface
+import numpy as np
+import copy
+
+
+class Sl1mSurfacePublisher():
+
+    def __init__(self, topic, queue_size=10):
+        # Initializing the publisher
+        self._pub = rospy.Publisher(topic, SetSurfaces, queue_size=queue_size)
+        self._surfacePlanner_iface = SurfacePlannerInterface()
+
+    def publish(self, t, set_surfaces):
+        msg = self._surfacePlanner_iface.writeToMessage(t, set_surfaces)
+        self._pub.publish(msg)
+
+
+class StepManagerPublisher():
+
+    def __init__(self, topic, queue_size=10):
+        # Initializing the publisher
+        self._pub = rospy.Publisher(topic, GaitStatusNewPhase, queue_size=queue_size)
+        self._stepmanager_iface = StepManagerInterface()
+
+    def publish(self, t, gait, target_foostep):
+        msg = self._stepmanager_iface.writeToMessage(t, gait, target_foostep)
+        self._pub.publish(msg)
+
+
+class SurfacePlannerInterface():
+
+    def __init__(self):
+        self._msg = SetSurfaces()
+
+    def writeToMessage(self, t, set_surfaces):
+        """ Write data to SurfacesSL1M message.
+
+        Args:
+            - t (float): time.
+            - set_surfaces (dict): Dictionnary object containing the next surfaces for each foot.
+        """
+        self._msg.header.stamp = rospy.Time(t)
+        self._msg.set_surfaces = []
+        for key in set_surfaces:
+            msg_tmp = FootSurfaces()
+            msg_tmp.name = key
+            for sf in set_surfaces[key]:
+                surface = ConvexSurface()
+                # Inequality matrix.
+                A = Float64MultiArray()
+                A.layout.dim.append(MultiArrayDimension())
+                A.layout.dim.append(MultiArrayDimension())
+                A.layout.dim[0].label = "height"
+                A.layout.dim[1].label = "width"
+                A.layout.dim[0].size = sf.A.shape[0]
+                A.layout.dim[1].size = sf.A.shape[1]
+                A.data = np.ravel(sf.A, order="C").tolist()
+                surface.A = A
+
+                # Inequality vector
+                surface.b = sf.b.tolist()
+
+                # Vertices matrix.
+                vert = Float64MultiArray()
+                vert.layout.dim.append(MultiArrayDimension())
+                vert.layout.dim.append(MultiArrayDimension())
+                vert.layout.dim[0].label = "height"
+                vert.layout.dim[1].label = "width"
+                vert.layout.dim[0].size = sf.vertices.shape[0]
+                vert.layout.dim[1].size = sf.vertices.shape[1]
+                vert.data = np.ravel(sf.vertices, order="C").tolist()
+                surface.vertices = vert
+
+                msg_tmp.surfaces.append(surface)
+
+            self._msg.set_surfaces.append(msg_tmp)
+
+        return self._msg
+
+    def writeFromMessage(self, msg):
+        """ Extract data from SurfacesSL1M message
+        """
+        set_surfaces = dict()
+        for foot_surfaces in msg.set_surfaces:
+            L = []
+            for sf in foot_surfaces.surfaces:
+                ia = sf.A.layout.dim[0].size
+                ja = sf.A.layout.dim[1].size
+                iv = sf.vertices.layout.dim[0].size
+                jv = sf.vertices.layout.dim[1].size
+                L.append(
+                    Surface(
+                        np.array(sf.A.data).reshape((ia, ja), order="C"), np.array(sf.b),
+                        np.array(sf.vertices.data).reshape((iv, jv), order="C")))
+            set_surfaces[foot_surfaces.name] = L
+
+        return set_surfaces
+
+
+class StepManagerInterface():
+
+    def __init__(self):
+        self._msg = GaitStatusNewPhase()
+        self._contact_names = ['LF_FOOT', 'RF_FOOT', 'LH_FOOT', 'RH_FOOT']  # Order of the feet in the surface planner.
+
+    def writeToMessage(self, t, gait, foot_pos):
+        """ Write data to ROS message FootStepStateSL1M.
+        Args:
+            - t (time): Current timing.
+            - gait (Array n_gait x 4): Next walking gait pattern.
+            - foot_pos (Array 3x4): Foot position for the next phase. (For the next phase of contact)
+        """
+        self._msg.header.stamp = rospy.Time(t)
+
+        # Gait matrix
+        gait_mat = Float64MultiArray()
+        gait_mat.layout.dim.append(MultiArrayDimension())
+        gait_mat.layout.dim.append(MultiArrayDimension())
+        gait_mat.layout.dim[0].label = "height"
+        gait_mat.layout.dim[1].label = "width"
+        gait_mat.layout.dim[0].size = gait.shape[0]
+        gait_mat.layout.dim[1].size = 4
+        gait_mat.data = np.ravel(gait, order="C").tolist()
+        self._msg.gait = gait_mat
+
+        # Target foosteps
+        for k in range(4):
+            self._msg.foot_pos[k].name = self._contact_names[k]
+            self._msg.foot_pos[k].position.x = foot_pos[0, k]
+            self._msg.foot_pos[k].position.y = foot_pos[1, k]
+            self._msg.foot_pos[k].position.z = foot_pos[2, k]
+
+        return self._msg
+
+    def writeFromMessage(self, msg):
+        """ Extract data from ROS message FootStepStateSL1M.
+        """
+        i = msg.gait.layout.dim[0].size
+        j = msg.gait.layout.dim[1].size
+        gait = np.reshape(msg.gait.data, ((i, j)), order="C")
+
+        foot_pos = np.zeros((3, 4))
+        for k in range(4):
+            self._msg.foot_pos[k].name = self._contact_names[k]
+            foot_pos[0, k] = self._msg.foot_pos[k].position.x
+            foot_pos[1, k] = self._msg.foot_pos[k].position.y
+            foot_pos[2, k] = self._msg.foot_pos[k].position.z
+
+        return gait, foot_pos
--- walkgen/__init__.py
+++ walkgen/__init__.py
@@ -3,5 +3,6 @@ from .FootStepPlanner import FootStepPlanner
 from .GaitManager import GaitManager
 from .FootStepTrajectory import FootStepTrajectory
 from .FootStepManager import FootStepManager
+from .WalkgenRosMessageConversion import *
 from params import WalkgenParams
 from .libwalkgen_pywrap import *
