848e20c6686c6d406fe19c2a6f3fb3464edc7925
thomasCbrs <thomas.corberes@gmail.com>
2022-02-16 15:29:34 +0000

    Fix import error

--- walkgen/__init__.py
+++ walkgen/__init__.py
@@ -4,5 +4,5 @@ from .GaitManager import GaitManager
 from .FootStepTrajectory import FootStepTrajectory
 from .FootStepManager import FootStepManager
 from .WalkgenRosMessageConversion import *
-from params import WalkgenParams
+from .params import WalkgenParams
 from .libwalkgen_pywrap import *
