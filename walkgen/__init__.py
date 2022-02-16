e4da7b7f3dd948812047893a85b4c16903c65603
thomasCbrs <thomas.corberes@gmail.com>
2022-02-16 15:10:06 +0000

    Adding WalkgenPArams class to __init__ file

--- walkgen/__init__.py
+++ walkgen/__init__.py
@@ -3,4 +3,5 @@ from .FootStepPlanner import FootStepPlanner
 from .GaitManager import GaitManager
 from .FootStepTrajectory import FootStepTrajectory
 from .FootStepManager import FootStepManager
+from params import WalkgenParams
 from .libwalkgen_pywrap import *
