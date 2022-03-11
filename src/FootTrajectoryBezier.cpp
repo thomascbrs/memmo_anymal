a3537e4c1c950ea79670861457668a3986b489e1
thomasCbrs <thomas.corberes@gmail.com>
2022-03-11 11:57:58 +0000

    [delate] Delate the walkgen package to separate into 3 modules

--- src/FootTrajectoryBezier.cpp
+++
@@ -1,8 +0,0 @@
-#include "FootTrajectoryBezier.hpp"
-
-FootTrajectoryBezier::FootTrajectoryBezier(){}
-
-
-void FootTrajectoryBezier::print_ok(int const& k){
-    std::cout << "k : " << k << std::endl;
-}
