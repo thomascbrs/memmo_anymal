8d7c15e2291a6fc6f9a001de06f2885b0ea94d78
thomasCbrs <thomas.corberes@gmail.com>
2022-03-14 10:42:30 +0000

    Add lowpass filter class

---
+++ walkgen_footstep_planner/python/walkgen_footstep_planner/tools/Filter.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 University of Edinburgh
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#  * Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#  * Neither the name of  nor the names of its contributors may be used to
+#    endorse or promote products derived from this software without specific
+#    prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import numpy as np
+from scipy.signal import butter
+
+class Filter():
+    """ Simple implementation of a lowpass filter.
+    """
+    def __init__(self, cutoff, fs, order = 1):
+        """
+        Args:
+            - cutoff (list): Cutoff frequencies for each axis.
+            - fs (float): Sampling frequency.
+            - order (int): Order of the filter.
+        """
+        _b, _a = [], []
+        for k in range(6):
+            b, a = self.butter_lowpass(cutoff[k], fs, order)
+            _b.append(b)
+            _a.append(a)
+        self._nb = len(_b[0])
+        self._na = len(_a[0])
+        self._b = np.array(_b)
+        self._a = np.array(_a)
+
+        self._x_queue, self._y_queue = [], []
+        self._is_initialized = False
+
+    def butter_lowpass(self, cutoff, fs, order=1):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        return b, a
+
+    def filter(self, q):
+        """ Filter.
+        Args:
+            - q (array x6): State to filter.
+
+        Returns:
+            - param1 (array x6): State filtered.
+        """
+        if not self._is_initialized:
+            self._x_queue = self._nb * [q]
+            self._y_queue = (self._na-1) * [q]
+            self._is_initialized = True
+        self._x_queue.pop()
+        self._x_queue.insert(0, q)
+
+        acc_ = 0
+        for i in range(self._nb):
+            acc_ += self._b[:,i] * self._x_queue[i]
+        for i in range(self._na-1):
+            acc_ -= self._a[:,i+1] * self._y_queue[i]
+        self._y_queue.pop()
+        self._y_queue.insert(0, acc_ / self._a[:,0])
+        return np.array(self._y_queue[0])
