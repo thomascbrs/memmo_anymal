30a3fa915e96224c751ee6dd6d1f30cc7abfebb2
thomasCbrs <thomas.corberes@gmail.com>
2023-02-13 16:05:59 +0000

    [update] Add a class to load surfaces from a folder containing .stl files

--- walkgen_surface_processing/python/walkgen_surface_processing/__init__.py
+++ walkgen_surface_processing/python/walkgen_surface_processing/__init__.py
@@ -1,4 +1,5 @@
 from .surface_detector import SurfaceDetector
+from .surface_loader import SurfaceLoader
 from .surface_processing import SurfaceProcessing
 from .params import SurfaceProcessingParams
 from .libwalkgen_surface_processing_pywrap import *
