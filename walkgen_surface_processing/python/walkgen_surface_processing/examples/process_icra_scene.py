1dafc9df7496c9f29e9a6f6a4f728b71d67b51df
thomasCbrs <thomas.corberes@gmail.com>
2023-04-07 17:50:52 +0100

    [update] Add ICRA loader

---
+++ walkgen_surface_processing/python/walkgen_surface_processing/examples/process_icra_scene.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pinocchio as pin
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+from walkgen_surface_processing.surface_loader import SurfaceLoader
+from walkgen_surface_processing.params import SurfaceProcessingParams
+from walkgen_surface_processing.tools.plot_tools import plot_surface
+
+# Parameters of the environment
+params = SurfaceProcessingParams()
+params.margin_inner = 0.0  # Inner margins [m].
+params.margin_outer = 0.0  # Inner margins [m].
+params.method_id = 1
+params.min_area = 0.0
+params.n_points = 10
+params.poly_size = 10
+
+# Rotation and translation of the environment
+translation = np.array([0., 0., 0.])  # translation
+rpy = np.array([0., 0., 0.])  # Roll,pitch & yaw.
+R = pin.rpy.rpyToMatrix(rpy)  # Rotation matrix
+
+# Extract surfaces from multiple .stl files.
+# 1 file corresponds to 1 surface (usefull for large areas)
+params.path = "/home/thomas_cbrs/devel/walkgen-environment-data/environments"
+params.stl = "/meshes/rendering/ICRA_parkour_g5r/"
+surface_detector = SurfaceLoader(params.path + params.stl, R, translation, "environment_", params, True)
+all_surfaces_processed = surface_detector.extract_surfaces()
+
+# Plot surfaces.
+fig = plt.figure(figsize=(10, 6))
+ax = plt.axes(projection='3d')
+for sf in surface_detector.all_surfaces.values():
+    plot_surface( np.array(sf), ax,color = "k")
+for sf in all_surfaces_processed.values():
+    plot_surface( np.array(sf), ax,color = "r")
+plt.title("Surfaces extracted from .stl file")
+plt.show()
--- walkgen_surface_processing/python/walkgen_surface_processing/surface_loader.py
+++ walkgen_surface_processing/python/walkgen_surface_processing/surface_loader.py
@@ -31,6 +31,7 @@ import numpy as np
 import trimesh
 from walkgen_surface_processing.tools.geometry_utils import order, apply_margin, align_points, process_surfaces
 from walkgen_surface_processing.params import SurfaceProcessingParams
+from walkgen_surface_processing.tools.SurfaceData import SurfaceData
 from os import listdir
 from os.path import isfile, join
 import copy
@@ -44,7 +45,8 @@ class SurfaceLoader:
                  orientation_matrix=np.identity(3),
                  translation=np.zeros(3),
                  prefix="environment_",
-                 params = None
+                 params = None,
+                 ICRA = False,
                  ):
         """ Load the surfaces.
 
@@ -68,30 +70,94 @@ class SurfaceLoader:
         self._min_area = self._params.min_area
         self._margin_inner = self._params.margin_inner
         self._margin_outer = self._params.margin_outer
+        
+        if not ICRA:
+            names = [f for f in listdir(folderpath) if isfile(join(folderpath, f))]
+            hmatrix = np.zeros((4,4))
+            hmatrix[:3,:3] = orientation_matrix[:,:]
+            hmatrix[:3,-1] = translation[:]
 
-        names = [f for f in listdir(folderpath) if isfile(join(folderpath, f))]
-        hmatrix = np.zeros((4,4))
-        hmatrix[:3,:3] = orientation_matrix[:,:]
-        hmatrix[:3,-1] = translation[:]
+            self.all_surfaces = dict()
+            # self.all_surfaces_reduced = dict()
+            for id,file in enumerate(names):
+                obj = trimesh.load_mesh(folderpath + file)
+                obj.apply_transform(hmatrix)
+                vert = order(np.array(obj.vertices))
+                filename = prefix + str(id)
+                self.all_surfaces[filename] = vert
+                # self.all_surfaces_reduced[filename] = apply_margin(np.array(vert),0.)
 
-        self.all_surfaces = dict()
-        # self.all_surfaces_reduced = dict()
-        for id,file in enumerate(names):
-            obj = trimesh.load_mesh(folderpath + file)
-            obj.apply_transform(hmatrix)
-            vert = order(np.array(obj.vertices))
-            filename = prefix + str(id)
-            self.all_surfaces[filename] = vert
-            # self.all_surfaces_reduced[filename] = apply_margin(np.array(vert),0.)
+            # Apply process to filter and decompose the surfaces to avoid overlap and apply a security margin.
+            surfaces = [np.array(sf) for sf in self.all_surfaces.values()]
+            self.surfaces_processed = process_surfaces(surfaces,
+                                                polySize=self._poly_size,
+                                                method=self._method_id,
+                                                min_area=self._min_area,
+                                                margin_inner=self._margin_inner,
+                                                margin_outer=self._margin_outer)
+        else:
+            # /surfaces_g0 --> wooden pallet
+            # /surfaces_g1 --> wooden pallets steps
+            # /surfaces_g2 --> small blocks
+            # /surfaces_g3 --> wood panels 20cm
+            # /surfaces_g4 --> crate
+            # /surfaces_g5 --> 2 wood panels != height
+            # /surfaces_rounded
+            # /surfaces_inclined
+            self.all_surfaces = dict()
+            self.surfaces_processed = []
+            # Whole scene
+            # folder_names = ["surfaces_g0/","surfaces_g1/","surfaces_g2/","surfaces_g3/","surfaces_g4/","surfaces_g5/","surfaces_rounded/", "surfaces_inclined/"]
+            # margins = [[0.01,0.],[0.02,0.05],[0.01,0.03],[0.005,0.03],[0.02,0.05],[0.02,0.05],[0.02,0.],[0.02,0.]] 
+            # starting at g2
+            # folder_names = ["surfaces_g0/","surfaces_g2/","surfaces_g3/","surfaces_g4/","surfaces_g5/","surfaces_rounded/", "surfaces_inclined/"]
+            # margins = [[0.01,0.],[0.01,0.03],[0.005,0.03],[0.02,0.05],[0.02,0.05],[0.03,0.],[0.03,0.]] 
+            # # starting at g3
+            # folder_names = ["surfaces_g0/","surfaces_g3/","surfaces_g4/","surfaces_g5/","surfaces_rounded/", "surfaces_inclined/"]
+            # margins = [[0.01,0.],[0.0,0.06],[0.02,0.05],[0.02,0.05],[0.03,0.],[0.03,0.]] 
+            # # starting at g4
+            # folder_names = ["surfaces_g0/","surfaces_g4/","surfaces_g5/","surfaces_rounded/", "surfaces_inclined/"]
+            # margins = [[0.01,0.],[0.04,0.07],[0.02,0.05],[0.03,0.],[0.03,0.]] 
+            # starting at g5
+            # folder_names = ["surfaces_g0/","surfaces_g5/","surfaces_rounded/", "surfaces_inclined/"]
+            # margins = [[0.01,0.],[0.02,0.05],[0.03,0.],[0.03,0.]]
+            # Whole scene rendering
+            folder_names = ["surfaces_g0/","surfaces_g1/","surfaces_g2/","surfaces_g3/","surfaces_g4/","surfaces_g5/","surfaces_rounded/", "surfaces_inclined/"]
+            margins = [[0.01,0.],[0.02,0.05],[0.01,0.03],[0.00,0.06],[0.04,0.07],[0.02,0.05],[0.03,0.],[0.03,0.]] 
+            for i,fname in enumerate(folder_names):
+                names = [f for f in listdir(folderpath+fname) if isfile(join(folderpath+fname, f))]
+                
+                hmatrix = np.zeros((4,4))
+                hmatrix[:3,:3] = orientation_matrix[:,:]
+                hmatrix[:3,-1] = translation[:]
 
-        # Apply process to filter and decompose the surfaces to avoid overlap and apply a security margin.
-        surfaces = [np.array(sf) for sf in self.all_surfaces.values()]
-        self.surfaces_processed = process_surfaces(surfaces,
-                                              polySize=self._poly_size,
-                                              method=self._method_id,
-                                              min_area=self._min_area,
-                                              margin_inner=self._margin_inner,
-                                              margin_outer=self._margin_outer)
+                all_surfaces_tmp = dict()
+                for id,file in enumerate(names):
+                    obj = trimesh.load_mesh(folderpath + fname +  file)
+                    obj.apply_transform(hmatrix)
+                    vert = order(np.array(obj.vertices))
+                    filename = prefix + "_gr" + str(i) + "_" + str(id)
+                    all_surfaces_tmp[filename] = vert
+                    self.all_surfaces[filename] = vert
+                    # self.all_surfaces_reduced[filename] = apply_margin(np.array(vert),0.)
+
+                # Apply process to filter and decompose the surfaces to avoid overlap and apply a security margin.
+                surfaces = [np.array(sf) for sf in all_surfaces_tmp.values()]
+            
+                surfaces_processed_tmp = process_surfaces(surfaces,
+                                                    polySize=self._poly_size,
+                                                    method=self._method_id,
+                                                    min_area=self._min_area,
+                                                    margin_inner=margins[i][0],
+                                                    margin_outer=margins[i][1])
+                    
+                for sf in surfaces_processed_tmp:
+                    if fname == "surfaces_g3/" :
+                        # Quick fix
+                        S = SurfaceData(sf,0.,0.)
+                        if S.h_mean > 0.10:
+                            continue
+                    self.surfaces_processed.append(sf)            
 
 
     def extract_surfaces(self):
